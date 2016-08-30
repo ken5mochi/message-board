54cc90fbf406619d5f6019db896004e908fcd92b
mochiken <kengo.mochizuki@gmail.com>
2016-08-30 01:53:43 +0000

    add edit and destroy action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
 
   def index
     @message = Message.new
@@ -11,14 +12,35 @@ class MessagesController < ApplicationController
       redirect_to root_path , notice: 'メッセージを保存しました'
     else
       @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      flash.now[:alert] = "メッセージの保存に失敗しました"
       render 'index'
     end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      flash.now[:alert] = "メッセージの更新に失敗しました"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
   end
 
 end
